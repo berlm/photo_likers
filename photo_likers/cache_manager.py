39a350e31d947b095751a7d32d0902d221179981
Mikhail Berlinkov <berlm@mail.ru>
2016-12-02 17:13:40 +0500

    refactoring

--- photo_likers/cache_manager.py
+++ photo_likers/cache_manager.py
@@ -35,6 +35,69 @@ class CustomPaginator:
             return 1
 
 
+class SortedListSearcher:
+    def __init__(self, page_number: int, tag_sorted_lists, tag_signs, search_cache_data):
+        self.tag_signs = tag_signs
+        self.tag_sorted_lists = tag_sorted_lists
+        self.page_number = page_number
+        self.cnt_lists = len(tag_signs)
+        self.res_values = []  # результирующие значения в пересечении
+        if search_cache_data is not None:
+            self.__num_pages = search_cache_data[CacheManager.NUM_PAGES_KEY]
+            checkpoints = search_cache_data[CacheManager.CHECKPOINTS_KEY]
+            checkpoint_index = min(max((page_number - 1) // CacheManager.SEARCH_CACHES_MEMORY_PAGE_STEP, 0),
+                                   len(checkpoints)-1)
+            self.cnt_found = checkpoint_index * CacheManager.SEARCH_CACHES_MEMORY_PAGE_STEP * PHOTOS_PER_PAGE
+            self.__pointers = checkpoints[checkpoint_index]
+        else:
+            self.__num_pages = None  # type: int  общее число страниц при заданном фильтре
+            self.cnt_found = 0  # число найденных фоток, подходящих под фильтр
+            self.__pointers = [0] * self.cnt_lists
+
+    def next(self):
+        if not self.has_next():
+            return None
+        # получаем значение хэша в первом списке и
+        # соответственно двигаем указатели в остальных
+        main_value = self.tag_sorted_lists[0][self.__pointers[0]]
+        for pointer_index in range(1, self.cnt_lists):
+            i = self.__pointers[pointer_index]
+            while self.tag_sorted_lists[pointer_index][i] > main_value:
+                i += 1
+            self.__pointers[pointer_index] = i
+            # если в соответствующем списке нет заданного фото и условие на тег
+            # включающее или наоборот, то это фото не подходит под фильтр
+            if (self.tag_sorted_lists[pointer_index][i] == main_value) != self.tag_signs[pointer_index]:
+                main_value = None
+                break
+        else:
+            if (self.page_number - 1) * PHOTOS_PER_PAGE <= self.cnt_found < self.page_number * PHOTOS_PER_PAGE:
+                self.res_values.append(main_value)
+            self.cnt_found += 1
+
+        # не забываем двигать указатель в первом списке
+        self.__pointers[0] += 1
+        return main_value
+
+    def has_next(self):
+        return self.__pointers[0] < len(self.tag_sorted_lists[0]) - 1
+
+    def get_found_pages_count(self):
+        num_pages, rest = divmod(self.cnt_found, PHOTOS_PER_PAGE)
+        if rest > 0:
+            num_pages += 1
+        return num_pages
+
+    def get_pages_number(self):
+        if self.__num_pages:
+            return self.__num_pages
+        else:
+            return self.get_found_pages_count()
+
+    def get_checkpoint(self):
+        return self.__pointers.copy()
+
+
 class CacheManager:
     """Класс менеджер для обновления и использования кэшей
 
@@ -92,76 +155,37 @@ class CacheManager:
             просматривать приходится фоток не более, чем при поиске 50-ти страниц
             (параметр SEARCH_CACHES_MEMORY_PAGE_STEP)
         """
-        # 1. Инициализация указателей на позиции в массивах и др.
-        num_pages = None  # type: int  общее число страниц при заданном фильтре
-        cnt_found = 0  # число найденных фоток, подходящих под фильтр
-        cnt = len(sorted_signed_tags)
-        pointers = [0] * cnt
-        search_cache_checkpoints = [pointers]  # checkpoint-ы найденных страниц для ускорения поиска
-        search_not_cached = True
         search_cache_key = CacheManager.get_search_cache_key(sorted_signed_tags=sorted_signed_tags,
                                                              sort_field=photo_cache.sort_field)
         search_cache_data = cache.get(search_cache_key)
-        # подгрузка данных о чекпоинтах (если есть)
-        if search_cache_data is not None:
-            num_pages = search_cache_data[CacheManager.NUM_PAGES_KEY]
-            checkpoints = search_cache_data[CacheManager.CHECKPOINTS_KEY]
-            checkpoint_index = min(max((page_number - 1) // CacheManager.SEARCH_CACHES_MEMORY_PAGE_STEP, 0),
-                                   len(checkpoints) - 1)
-            if checkpoint_index > 0:
-                cnt_found = checkpoint_index * CacheManager.SEARCH_CACHES_MEMORY_PAGE_STEP * PHOTOS_PER_PAGE
-                pointers = checkpoints[checkpoint_index]
-            search_not_cached = False
-
         tag_signs = [tag_id > 0 for tag_id in sorted_signed_tags]
         tag_sorted_lists = [photo_cache.get_photo_hashes_for_tag(abs(tag_id)) for tag_id in
                             sorted_signed_tags]
-        res_list_photo_ids = []  # результирующие фотки (id)
-        while (cnt_found < page_number * PHOTOS_PER_PAGE or search_not_cached) \
-                and (pointers[0] < len(tag_sorted_lists[0]) - 1):
+        searcher = SortedListSearcher(page_number=page_number, tag_sorted_lists=tag_sorted_lists, tag_signs=tag_signs,
+                                      search_cache_data=search_cache_data)
+        search_cache_checkpoints = [searcher.get_checkpoint()]
+        search_not_cached = search_cache_data is None
+        while (searcher.cnt_found < page_number * PHOTOS_PER_PAGE or search_not_cached) \
+                and searcher.has_next():
+            if searcher.next() is not None and search_not_cached \
+                    and searcher.cnt_found % (PHOTOS_PER_PAGE * CacheManager.SEARCH_CACHES_MEMORY_PAGE_STEP) == 0:
+                search_cache_checkpoints.append(searcher.get_checkpoint())
 
-            # получаем значение хэша в первом списке и
-            # соответственно двигаем указатели в остальных
-            main_value = tag_sorted_lists[0][pointers[0]]
-            for pointer_index in range(1, cnt):
-                i = pointers[pointer_index]
-                while tag_sorted_lists[pointer_index][i] > main_value:
-                    i += 1
-                pointers[pointer_index] = i
-                # если в соответствующем списке нет заданного фото и условие на тег
-                # включающее или наоборот, то это фото не подходит под фильтр
-                if (tag_sorted_lists[pointer_index][i] == main_value) != tag_signs[pointer_index]:
-                    break
-            else:
-                # если по break не вышли, значит фото подходит под фильтр
-                photo_id = photo_cache.get_photo_id_by_hash(main_value)
-                if photo_id > 0:
-                    if (page_number - 1) * PHOTOS_PER_PAGE <= cnt_found < page_number * PHOTOS_PER_PAGE:
-                        res_list_photo_ids.append(photo_id)
-                    cnt_found += 1
-                    if search_not_cached:
-                        if cnt_found % (PHOTOS_PER_PAGE * CacheManager.SEARCH_CACHES_MEMORY_PAGE_STEP) == 0:
-                            pointers_copy = pointers.copy()
-                            pointers_copy[0] += 1
-                            search_cache_checkpoints.append(pointers_copy)
-
-            # не забываем двигать указатель в первом списке
-            pointers[0] += 1
-
+        res_list_photo_ids = [photo_cache.get_photo_id_by_hash(hash_value) for hash_value in searcher.res_values]
         if search_not_cached:
-            num_pages = CacheManager.save_search_cache(search_cache_key, search_cache_checkpoints, cnt_found)
+            CacheManager.save_search_cache(search_cache_key, search_cache_checkpoints,
+                                           searcher.get_found_pages_count())
 
-        # список фото получаем и переупорядочиваем одним запросом, чтобы не делать 20 запросов к БД
+        # список фото получаем и переупорядочиваем одним запросом,
+        # чтобы не делать 20 запросов к БД
         res_list = sorted(Photo.objects.filter(id__in=res_list_photo_ids).all(),
                           key=lambda photo: res_list_photo_ids.index(photo.id))
 
         return Page(object_list=res_list, number=page_number,
-                    paginator=CustomPaginator(num_pages=num_pages))
+                    paginator=CustomPaginator(num_pages=searcher.get_pages_number()))
 
     @staticmethod
-    def save_search_cache(search_cache_key, search_cache_checkpoints, cnt_found):
-        num_pages, rest = divmod(cnt_found, PHOTOS_PER_PAGE)
-        if rest > 0: num_pages += 1
+    def save_search_cache(search_cache_key, search_cache_checkpoints, num_pages: int):
         cache.set(
             search_cache_key,
             {CacheManager.NUM_PAGES_KEY: num_pages, CacheManager.CHECKPOINTS_KEY: search_cache_checkpoints},
